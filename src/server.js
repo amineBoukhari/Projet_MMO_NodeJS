6794fd9a79b3d5d28d1a7b44117eb6afa2952e5a
Diarapak <krobpierre0@gmail.com>
2025-11-24 09:59:34 +0100

    fix associate

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from 'http';
 import app from './app.js'; // Application Express
 import sequelize from './config/sequelize.js'; // Connexion Sequelize
-import models from './models/index.js'; // Import des modèles et associations
+import './models/associate.js'; // Import des associations
 import logger from './config/logger.js'; // Logger Winston
 import config from './config/config.js'; // Configuration (PORT, etc.)
 import initialData from './config/initialData.js'; // Fonction pour initialiser les données
